c6d48e50552d92667229af0a542a1263beb67f9d
Matt Nawrocki <mjnrock@umich.edu>
2021-05-10 11:08:04 -0400

    init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
+import React from "react";
+import { Segment } from "semantic-ui-react";
+
+import ReactNetwork from "./agency/ReactNetwork";
+import { useNetwork, } from "./agency/useNetwork"
+
+export const Context = React.createContext();
+
+const network = new ReactNetwork({
+    cats: 54,
+});
+
 function App() {
+    const { state, dispatch } = useNetwork(network);
+
     return (
-        <div className="App">
-            Sup
-        </div>
+        <Context.Provider value={{ network }}>
+            <Segment>
+                {
+                    JSON.stringify(state)
+                }
+            </Segment>
+        </Context.Provider>
     );
-}
+};
 
 export default App;
